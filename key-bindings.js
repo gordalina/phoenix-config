bc2a1c3e7e34ca24d250a888e5319912b2086753
Samuel Gordalina <samuel.gordalina@gmail.com>
2019-05-29 11:57:41 -0700

    Add thirds keyboard shortcuts

--- key-bindings.js
+++ key-bindings.js
@@ -7,6 +7,12 @@ const handles = {
   maximize: new Key('m', [ 'ctrl', 'alt' ], () => Window.focused().maximize()),
 
   // move window between screens
-  leftScreen: new Key('left', [ 'ctrl', 'alt', 'cmd' ], () => moveToScreen({offset: -1})),
-  rightScreen: new Key('right', [ 'ctrl', 'alt', 'cmd' ], () => moveToScreen({offset: 1})),
+  leftScreen: new Key('left', [ 'ctrl', 'alt', 'cmd' ], () => moveToScreen({offset: 1})),
+  rightScreen: new Key('right', [ 'ctrl', 'alt', 'cmd' ], () => moveToScreen({offset: -1})),
+
+  left1: new Key('left', [ 'ctrl', 'alt', 'shift', 'cmd' ], () => setFrame(getPosition('left-1'))),
+  left2: new Key('left', [ 'ctrl', 'alt', 'shift' ], () => setFrame(getPosition('left-2'))),
+
+  right1: new Key('right', [ 'ctrl', 'alt', 'shift', 'cmd' ], () => setFrame(getPosition('right-1'))),
+  right2: new Key('right', [ 'ctrl', 'alt', 'shift' ], () => setFrame(getPosition('right-2'))),
 };
--- position.js
+++ position.js
@@ -11,5 +11,11 @@ function getPosition(name) {
     case 'top-right': return { x: .5, y: 0, width: .5, height: .5 };
     case 'bottom-right': return { x: .5, y: .5, width: .5, height: .5 };
     case 'bottom-left': return { x: 0, y: .5, width: .5, height: .5 };
+
+    /* THIRDS */
+    case 'left-1': return { x: 0, y: 0, width: 1/3, height: 1 };
+    case 'left-2': return { x: 0, y: 0, width: 2/3, height: 1 };
+    case 'right-1': return { x: 2/3, y: 0, width: 1/3, height: 1 };
+    case 'right-2': return { x: 1/3, y: 0, width: 2/3, height: 1 };
   }
 }
